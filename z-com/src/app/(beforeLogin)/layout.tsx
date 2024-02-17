795e18d0d130d03ad4e0d741538444b0f7383a8a
beeguriri <kkamboting@gmail.com>
2024-02-17 12:36:56 +0900

    필요없는 파일 삭제

--- z-com/src/app/(beforeLogin)/layout.tsx
+++ z-com/src/app/(beforeLogin)/layout.tsx
@@ -1,10 +1,10 @@
 import {ReactNode} from "react";
-import styles from "@/app/page.module.css";
+// import styles from "@/app/page.module.css";
 
 type Props = { children: ReactNode, modal: ReactNode };
 export default function BeforeLoginLayout({ children, modal }: Props) {
   return (
-    <div className={styles.container}>
+    <div>
       {children}
       {modal}
     </div>
