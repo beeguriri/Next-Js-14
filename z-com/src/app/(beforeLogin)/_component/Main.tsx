fd9b70c51152800b5820ab69015a212682b53d91
beeguriri <kkamboting@gmail.com>
2024-02-17 12:31:11 +0900

    리다이렉트 문제 해결

---
+++ z-com/src/app/(beforeLogin)/_component/Main.tsx
@@ -0,0 +1,21 @@
+import Image from "next/image";
+import styles from "./main.module.css";
+import Link from "next/link";
+import myLogo from "../../../../public/myLogo.png";
+
+export default function Main() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.left}>
+        <Image src={myLogo} alt="logo" width={300}/>
+      </div>
+      <div className={styles.right}>
+        <h1>지금 일어나고 있는 일</h1>
+        <h3>지금 가입하세요.</h3>
+        <Link href="/i/flow/signup" className={styles.signup}>계정 만들기</Link>
+        <h3>이미 가입 하셨나요?</h3>
+        <Link href="/login" className={styles.login}>로그인</Link>
+      </div>
+    </div>
+  )
+}
