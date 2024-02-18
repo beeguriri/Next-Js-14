63c00461f74fdad526862970de7bed058eb0fb95
beeguriri <kkamboting@gmail.com>
2024-02-18 18:39:12 +0900

    쪽지 목록 만들기

--- z-com/src/app/(afterLogin)/messages/page.tsx
+++ z-com/src/app/(afterLogin)/messages/page.tsx
@@ -1,10 +1,21 @@
+import { faker } from '@faker-js/faker';
+import style from './message.module.css';
+import Room from './_component/Room';
+
 export default function Page() {
   return (
-    <main>
-      쪽지
+    <main className={style.main}>
+      <div className={style.header}>
+        <h3>쪽지</h3>
+      </div>
+      <Room />
+      <Room />
+      <Room />
+      <Room />
+      <Room />
     </main>
   )
 }
 
 //https://twitter.com/messages
-//엄밀히 말하면 messages/[messagesID] 존재함!
+//클릭하면 https://twitter.com/messages/[roomId]
