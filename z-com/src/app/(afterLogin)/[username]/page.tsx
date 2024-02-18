44f8cd8f19f0cb2dccb36b2abce1f548f78b6d0e
beeguriri <kkamboting@gmail.com>
2024-02-18 14:22:43 +0900

    profile page default layout

--- z-com/src/app/(afterLogin)/[username]/page.tsx
+++ z-com/src/app/(afterLogin)/[username]/page.tsx
@@ -1,7 +1,45 @@
+import BackButton from '../_component/BackButton';
+import Post from '../_component/Post';
+import style from './profile.module.css';
+
 export default function Profile() {
+
+  // 임시로 내 정보 있는것처럼
+  const me = { 
+    id: 'beeguri',
+    nickname: '비그링',
+    image: '/profile.jpg',
+  }
+
+  //백버튼, 아이디, 게시물 수
+  //프로필 사진
+  //아이디와 닉네임
+  //가입일
+  //...
+  //게시물 관련
   return (
-    <main>
-      프로필
+    <main className={style.main}>
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>{me.nickname}</h3>
+      </div>
+      <div className={style.userZone}>
+        <div className={style.userImage}>
+          <img src={me.image} alt={me.id} />
+        </div>
+        <div className={style.userName}>
+          <div>{me.nickname}</div>
+          <div>@{me.id}</div>
+        </div>
+        <button className={style.followButton}>프로필 설정하기</button>
+      </div>
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
     </main>
   )
 }
---
+++ z-com/src/app/(afterLogin)/_component/BackButton.tsx
@@ -0,0 +1,18 @@
+"use client"
+
+import style from "./backButton.module.css";
+
+export default function BackButton() {
+
+  const onClick = () => {};
+
+  return (
+    <button className={style.backButton} onClick={onClick}>
+      <svg width={24} viewBox="0 0 24 24" aria-hidden="true">
+        <g>
+          <path d="M7.414 13l5.043 5.04-1.414 1.42L3.586 12l7.457-7.46 1.414 1.42L7.414 11H21v2H7.414z"></path>
+        </g>
+      </svg>
+    </button>
+  )
+}
