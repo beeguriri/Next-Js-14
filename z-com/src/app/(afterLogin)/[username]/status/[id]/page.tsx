6c30bc129f8c343523b125911773530bd51027e6
beeguriri <kkamboting@gmail.com>
2024-02-18 16:38:10 +0900

    post 상세보기

--- z-com/src/app/(afterLogin)/[username]/status/[id]/page.tsx
+++ z-com/src/app/(afterLogin)/[username]/status/[id]/page.tsx
@@ -1,8 +1,32 @@
+import BackButton from "@/app/(afterLogin)/_component/BackButton";
+import style from "./singlePost.module.css";
+import Post from "@/app/(afterLogin)/_component/Post";
+import CommentForm from "./_component/CommentForm";
+
+
+// 원본 게시글
+// 답글 게시하기
+// 답글
 export default function SinglePost() {
   return (
-    <main>
-      게시글 상세
-    </main>
+    <div className={style.main}>
+      {/* 헤더 영역 */}
+      <div className={style.header}>
+        <BackButton />
+        <h3 className={style.headerTitle}>게시하기</h3>
+      </div>
+      {/* 원본게시글 */}
+      <Post />
+      {/* comment 폼 */}
+      <CommentForm />
+      {/* comment list => 얘도 post임 */}
+      <div>
+        <Post />
+        <Post />
+        <Post />
+        <Post />
+      </div>
+    </div>
   )
 }
 
