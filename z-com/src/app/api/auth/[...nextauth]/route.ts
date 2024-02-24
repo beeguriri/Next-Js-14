c336d210686a3807bed1e1c7e94c57e0cbcb1ac3
beeguriri <kkamboting@gmail.com>
2024-02-24 12:32:13 +0900

    클라이언트 사이드에서 로그인하기

--- z-com/src/app/api/auth/[...nextauth]/route.ts
+++ z-com/src/app/api/auth/[...nextauth]/route.ts
@@ -1,3 +1,2 @@
-export { GET, POST } from '@/app/auth';
-
+export { GET, POST } from '@/auth';
 //auth.ts가 대신 관리 해줌
--- z-com/src/mocks/handlers.ts
+++ z-com/src/mocks/handlers.ts
@@ -1,21 +1,16 @@
-import { http, HttpResponse } from 'msw'
+import { http, HttpResponse } from 'msw';
+import { faker } from '@faker-js/faker';
+
+
+const User = [
+	{id: 'elonmusk', nickname: 'Elon Musk', image: '/elon_temp.jpg'},
+	{id: 'beeguri', nickname: '비그링', image: '/profile.jpg'},
+	{id: 'wendy', nickname: '웬디', image: faker.image.avatar()},
+]
 
 //공식문서 https://mswjs.io/docs/basics/mocking-responses 참조
 export const handlers = [
 	//HttpResponse.json(body, init)
-	http.post('', () => {
-		// Note that you DON'T have to stringify the JSON!
-		return HttpResponse.json(
-			{
-				id: 'abc-123',
-				title: 'Modern Testing Practices',
-			}, {
-			headers: {
-				'Content-Type': 'text/plain',
-			},
-		}
-		)
-	}),
 
 	//회원가입
 	http.post('api/users', async () => {
@@ -36,5 +31,15 @@ export const handlers = [
 		// 	status: 403,
 		// 	statusText: 'user_exists',
 		// })
+	}),
+
+	//로그인
+	http.post('/api/login', () => {
+		console.log('로그인');
+		return HttpResponse.json(User[1], {
+		  headers: {
+			'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
+		  }
+		})
 	}),
 ]
