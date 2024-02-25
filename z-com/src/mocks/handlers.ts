7e4819ae9a80017e680326e001924ae78edac18c
beeguriri <kkamboting@gmail.com>
2024-02-25 11:14:06 +0900

    mock api 작성

--- z-com/src/mocks/handlers.ts
+++ z-com/src/mocks/handlers.ts
@@ -1,45 +1,112 @@
 import { http, HttpResponse } from 'msw';
 import { faker } from '@faker-js/faker';
 
+function generateDate() {
+  const lastWeek = new Date(Date.now());
+  lastWeek.setDate(lastWeek.getDate() - 7);
+  return faker.date.between({
+    from: lastWeek,
+    to: Date.now(),
+  });
+}
 
 const User = [
-	{id: 'elonmusk', nickname: 'Elon Musk', image: '/elon_temp.jpg'},
-	{id: 'beeguri', nickname: '비그링', image: '/profile.jpg'},
-	{id: 'wendy', nickname: '웬디', image: faker.image.avatar()},
+  { id: 'elonmusk', nickname: 'Elon Musk', image: '/elon_temp.jpg' },
+  { id: 'beeguri', nickname: '비그링', image: '/profile.jpg' },
+  { id: 'wendy', nickname: '웬디', image: faker.image.avatar() },
 ]
 
 //공식문서 https://mswjs.io/docs/basics/mocking-responses 참조
 export const handlers = [
-	//HttpResponse.json(body, init)
+  //HttpResponse.json(body, init)
 
-	//회원가입
-	http.post('api/users', async () => {
+  //회원가입
+  http.post('api/users', async () => {
 
-		console.log('회원가입');
+    console.log('회원가입');
 
-		//성공했을 때
-		return new HttpResponse('ok', {
-			status: 200,
-			statusText: 'ok',
-			headers: {
-				'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/;Max-Age=0'
-			}
-		})
+    //성공했을 때
+    //그리고 보낼 데이터가 없을 때 new HttpResponse 사용
+    return new HttpResponse('ok', {
+      status: 200,
+      statusText: 'ok',
+      headers: {
+        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/;Max-Age=0'
+      }
+    })
 
-		//실패 했을 때
-		// return new HttpResponse(null, {
-		// 	status: 403,
-		// 	statusText: 'user_exists',
-		// })
-	}),
+    //실패 했을 때
+    // return new HttpResponse(null, {
+    // 	status: 403,
+    // 	statusText: 'user_exists',
+    // })
+  }),
 
-	//로그인
-	http.post('/api/login', () => {
-		console.log('로그인');
-		return HttpResponse.json(User[1], {
-		  headers: {
-			'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
-		  }
-		})
-	}),
+  //로그인
+  http.post('/api/login', () => {
+    console.log('로그인');
+    return HttpResponse.json(User[1], {
+      headers: {
+        'Set-Cookie': 'connect.sid=msw-cookie;HttpOnly;Path=/'
+      }
+    })
+  }),
+
+  //post 가져오기
+  http.get('/api/postRecommends', ({ request }) => {
+    const url = new URL(request.url);
+    const cursor = parseInt(url.searchParams.get('cursor') as string) || 0;
+
+    return HttpResponse.json(
+      [
+        {
+          postId: cursor + 1,
+          User: User[0],
+          content: `${cursor + 1} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [{ imageId: 1, link: faker.image.urlLoremFlickr() }],
+          createdAt: generateDate(),
+        },
+        {
+          postId: cursor + 2,
+          User: User[0],
+          content: `${cursor + 2} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+        {
+          postId: cursor + 3,
+          User: User[0],
+          content: `${cursor + 3} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [],
+          createdAt: generateDate(),
+        },
+        {
+          postId: cursor + 4,
+          User: User[0],
+          content: `${cursor + 4} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+            { imageId: 3, link: faker.image.urlLoremFlickr() },
+            { imageId: 4, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+        {
+          postId: cursor + 5,
+          User: User[0],
+          content: `${cursor + 5} Z.com is so marvelous. I'm gonna buy that.`,
+          Images: [
+            { imageId: 1, link: faker.image.urlLoremFlickr() },
+            { imageId: 2, link: faker.image.urlLoremFlickr() },
+            { imageId: 3, link: faker.image.urlLoremFlickr() },
+          ],
+          createdAt: generateDate(),
+        },
+      ]
+    )
+  }),
 ]
