57a38d83ef03e35296cd8e3d587be5d99ae0f5c1
beeguriri <kkamboting@gmail.com>
2024-03-05 16:03:09 +0900

    사진 클릭하면 이미지존 뜨는 컴포넌트 분리

--- z-com/src/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/page.tsx
+++ z-com/src/app/(afterLogin)/@modal/[username]/status/[id]/photo/[photoId]/page.tsx
@@ -1,48 +1,61 @@
-import Post from '@/app/(afterLogin)/_component/Post';
 import PhotoModalCloseButton from './_component/PhotoModalCloseButton';
 import style from './photoModal.module.css';
 import CommentForm from '@/app/(afterLogin)/[username]/status/[id]/_component/CommentForm';
-import { faker } from '@faker-js/faker';
-import ActionButtons from '@/app/(afterLogin)/_component/ActionButtons';
+import { HydrationBoundary, QueryClient, dehydrate } from '@tanstack/react-query';
+import SinglePost from '@/app/(afterLogin)/[username]/status/[id]/_component/SinglePost';
+import getSinglePost from '@/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost';
+import getComments from '@/app/(afterLogin)/[username]/status/[id]/_lib/getComments';
+import Comments from '@/app/(afterLogin)/[username]/status/[id]/_component/Comments';
+import ImageZone from './_component/ImageZone';
 
 //화면 삼분할
 //첫번째 분할에 버튼
 //두번째 분할에 사진 및 action buttons
 //세번쨰 분할에 사진 없는 포스트, action buttons, comment form, 답글 목록들
-export default function PhotoModal() {
 
-  const target = {
-    imageId: 1, 
-    link: faker.image.urlLoremFlickr(),
-  }
+type Props = {
+  params: { id: string }
+}
+
+
+export default async function PhotoModal({ params }: Props) {
+
+  const { id } = params;
+  const queryClient = new QueryClient();
+
+  await queryClient.prefetchQuery(
+    {
+      queryKey: ['posts', id],
+      queryFn: getSinglePost,
+    }
+  );
+
+  await queryClient.prefetchQuery(
+    {
+      queryKey: ['posts', id, 'comments'],
+      queryFn: getComments,
+    }
+  );
+
+  const dehydratedState = dehydrate(queryClient);
 
   return (
     <div className={style.container}>
-      {/* 닫기 버튼 */}
-      <PhotoModalCloseButton />
-      {/* 이미지 영역 */}
-      <div className={style.imageZone}>
-        <img src={target.link} />
-        <div className={style.image} style={{backgroundImage: `url(${target.link})`}} />
-        <div className={style.buttonZone}>
-          <div className={style.buttonInner}>
-            <ActionButtons photoModal />
-          </div>
+      <HydrationBoundary state={dehydratedState}>
+
+        {/* 닫기 버튼 */}
+        <PhotoModalCloseButton />
+        {/* 이미지 영역 */}
+        <ImageZone id={id} />
+
+        {/* ⬇️ TODO: 오른쪽 모달 닫는 용도의 버튼 */}
+        {/* 오른쪽 영역 */}
+        <div className={style.commentZone}>
+          <SinglePost id={id} noImage />
+          <CommentForm id={id} />
+          <Comments id={id} />
         </div>
-      </div>
-
-      {/* ⬇️ TODO: 오른쪽 모달 닫는 용도의 버튼 */}
-
-      {/* 오른쪽 영역 */}
-      <div className={style.commentZone}>
-        <Post noImage />
-        <CommentForm />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-        <Post />
-      </div>
+      </HydrationBoundary>
     </div>
   );
 }
