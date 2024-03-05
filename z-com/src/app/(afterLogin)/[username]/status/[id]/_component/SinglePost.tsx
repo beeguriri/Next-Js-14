51860a310c869c949e0a2cf50c6e922d6e0f70a5
beeguriri <kkamboting@gmail.com>
2024-03-05 15:20:04 +0900

    post 한개 상세보기

---
+++ z-com/src/app/(afterLogin)/[username]/status/[id]/_component/SinglePost.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { Post as IPost} from '@/model/Post';
+import { useQuery, useQueryClient } from '@tanstack/react-query';
+import getSinglePost from '../_lib/getSinglePost';
+import Post from '@/app/(afterLogin)/_component/Post';
+import { error } from 'console';
+
+type Props = {
+  id: string,
+}
+
+export default function SinglePost({id}: Props) {
+
+  const { data: post, error } = useQuery<IPost, Object, IPost, [_1: string, _2: string]>({
+    queryKey: ['posts', id],
+    queryFn: getSinglePost,
+    staleTime: 60*1000, //default 0ms (fresh => stale time)
+    gcTime: 5*60*1000, //default 5*60*1000 ms (5분)
+  });
+
+
+  if(error) {
+    return (
+      <div style={{
+        height: 100,
+        alignItems: 'center',
+        fontSize: 31,
+        fontWeight: 'bold',
+        justifyContent: 'center',
+        display: 'flex'
+      }}
+      >
+        게시글을 찾을 수 없습니다.</div>
+    )
+  }
+
+  if(!post) {
+    //로딩중
+    return null; 
+  }
+  return <Post key={post.postId} post={post}/>
+}
---
+++ z-com/src/app/(afterLogin)/[username]/status/[id]/_lib/getSinglePost.ts
@@ -0,0 +1,24 @@
+import { Post } from "@/model/Post";
+import { QueryFunction } from "@tanstack/react-query";
+
+const getSinglePost: QueryFunction<Post, [_1: string, _2: string]> 
+    = async({ queryKey }) => {
+
+  const [_1, id] = queryKey;
+  const response = await fetch(
+    `http://localhost:9090/api/posts/${id}`,
+    {
+      next: {
+        tags: ['posts', id],
+      },
+      cache: 'no-store',
+    }
+  );
+
+  if(!response.ok)
+    throw new Error('failed to fetch data');
+
+  return response.json();
+}
+
+export default getSinglePost;
